10553d6d7e940fbc055535447548d9f9b9f3a659
Matin <olayemiolamilekan05@gmail.com>
2025-08-28 05:19:14 +0100

    Implement Simple Quiz Logic

---
+++ quiz.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function checkAnswer() {
+        const correctAnswer = '4';
+        const feedback = document.getElementById('feedback');
+        const option = document.querySelector('[name="quiz"]:checked');
+        const userAnswer = option.value;
+    
+        // Compare Answer
+        if (userAnswer == correctAnswer) {
+            feedback.textContent = "Correct! Well done.";
+        } else {
+            feedback.textContent = "That's incorrect. Try again!";
+        }
+    }
+    
+        //submit
+        document.getElementById('submit-answer').addEventListener('click', checkAnswer);    
+})
